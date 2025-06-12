dc404aede089bad027b1298b080d031546e220ca
Sharayu Mujumale <sharayu.mujumale@icodexsolutions.com>
2025-06-12 14:58:44 +0530

    Latest

---
+++ Clothify.Order.Application/Services/OrderService.cs
@@ -0,0 +1,13 @@
+﻿using Clothify.Order.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clothify.Order.Application.Services
+{
+    public class OrderService : IOrderService
+    {
+    }
+}
---
+++ Clothify.Order.Infrastructure/Data/OrderDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clothify.Order.Infrastructure.Data
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options) { }
+     
+       public DbSet<OrderEntity> Orders { get; set; }
+    }
+}
